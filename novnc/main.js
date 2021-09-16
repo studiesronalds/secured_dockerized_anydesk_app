ee335c9818b9315b91b395c849f902feed76d792
Ronalds Sovas <sovas@greenbox.lv>
2021-09-16 08:19:04 +0300

    proxy param is optional

--- novnc/main.js
+++ novnc/main.js
@@ -1,16 +1,23 @@
 //if env not 
-if (!process.env.PROXY_URL || !process.env.PROCESS_URL){
+if (!process.env.PROCESS_URL){
 	process.exit();
 }
 
 const Nightmare = require('nightmare');
-nightmare = Nightmare({
-	switches: {
-        'proxy-server': process.env.PROXY_URL,
-        'ignore-certificate-errors': true
-    },
-	show: true
-});
+
+if (!process.env.PROXY_URL){
+	nightmare = Nightmare({
+		show: true
+	});
+} else {
+	nightmare = Nightmare({
+		switches: {
+	        'proxy-server': process.env.PROXY_URL,
+	        'ignore-certificate-errors': true
+	    },
+		show: true
+	});
+}
 
 nightmare.viewport(1080, 1920) 
 .goto(process.env.PROCESS_URL)
