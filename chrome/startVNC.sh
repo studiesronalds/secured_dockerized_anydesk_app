a5caac377495e7b301c5964e5316fdcd91810c91
Ubuntu <ubuntu@ip-172-26-0-147.eu-central-1.compute.internal>
2021-09-26 16:07:41 +0000

    added `aw swap fixtue`

--- chrome/startVNC.sh
+++ chrome/startVNC.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+export CHROME_LOG_FILE=/tmp/log.txt;
+
+
 if [[ -z "${PROXY_URL}" ]]; then
-	google-chrome --no-sandbox \
+	google-chrome --no-sandbox --enable-logging --v=1 \
 				  --no-first-run \
+				  --disable-dev-shm-usage --disable-software-rasterizer --disable-gpu --use-fake-ui-for-media-stream --use-fake-device-for-media-stream --disable-sync \
 				  --no-default-browser-check \
 				  --ignore-certificate-errors \
 				  --ignore-urlfetcher-cert-requests \
@@ -9,8 +13,9 @@ if [[ -z "${PROXY_URL}" ]]; then
 				  --no-sandbox --kiosk "$PROCESS_URL" \
 				  -inkognito
 else
-	google-chrome --no-sandbox \
+	google-chrome --no-sandbox --enable-logging --v=1 \
 				  --no-first-run \
+				  --disable-dev-shm-usage --disable-software-rasterizer --disable-gpu --use-fake-ui-for-media-stream --use-fake-device-for-media-stream --disable-sync \
 				  --no-default-browser-check \
 				  --ignore-certificate-errors \
 				  --ignore-urlfetcher-cert-requests \
